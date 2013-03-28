c6728d65ab781e0336aed824f31365a4e0996096
Steve Pomeroy <steve@staticfree.info>
2013-03-28 15:21:13 -0400

    Sets title on Cast List activity

--- src/edu/mit/mobile/android/locast/example/app/CastListActivity.java
+++ src/edu/mit/mobile/android/locast/example/app/CastListActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.text.TextUtils;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 
+import edu.mit.mobile.android.locast.data.tags.TaggableUtils;
 import edu.mit.mobile.android.locast.example.R;
 
 public class CastListActivity extends SherlockFragmentActivity {
@@ -20,8 +22,8 @@ public class CastListActivity extends SherlockFragmentActivity {
     private Uri mCasts;
 
     @Override
-    protected void onCreate(Bundle arg0) {
-        super.onCreate(arg0);
+    protected void onCreate(Bundle savedState) {
+        super.onCreate(savedState);
 
         setContentView(R.layout.activity_cast_list);
 
@@ -40,10 +42,36 @@ public class CastListActivity extends SherlockFragmentActivity {
                 ft.add(R.id.casts, CastListFragment.instantiate(mCasts));
             }
 
+            setTitleFromUri(mCasts);
+
             ft.commit();
         } else {
             finish();
         }
+    }
+
+    public void setTitleFromUri(Uri castDir) {
+        final String tags = castDir.getQueryParameter(TaggableUtils.QUERY_PARAMETER_TAGS);
+        CharSequence title;
+        if (tags != null) {
+            final StringBuilder sb = new StringBuilder();
+            final String[] tagList = TextUtils.split(tags, ",");
+
+            for (int i = 0; i < tagList.length; i++) {
+                if (i > 0) {
+                    sb.append(getText(R.string.title_activity_cast_list_tags_comma));
+                    if (i == tagList.length - 1) {
+                        sb.append(getText(R.string.title_activity_cast_list_tags_and));
+                    }
+                }
+                sb.append(getString(R.string.title_activity_cast_list_tags_quote, tagList[i]));
+            }
+
+            title = getString(R.string.title_activity_cast_list_tags, sb.toString());
+        } else {
+            title = getText(R.string.title_activity_cast_list_all);
+        }
+        setTitle(title);
     }
 
     @Override
